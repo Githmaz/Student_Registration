c92380d8a0b8f8f51384bf753c4f681302b5cfa5
Githmaz <Githmaz1@gmail.com>
2023-12-30 09:50:11 +0530

    Replace promise-based with async/await in getAllStudents

--- Backend-Express/src/services/studentService.js
+++ Backend-Express/src/services/studentService.js
@@ -1,15 +1,15 @@
 const db = require('../config/dbconfig')
 
  //------------------- Get all students -------------------//
-const getAllStudents = async () => {
-    return db.query('SELECT * FROM students')
-      .then(([rows]) => rows)
-      .catch(error => {
-        console.error('Error fetching students:', error);
-        throw new Error('Internal Server Error');
-      });
+ const getAllStudents = async () => {
+    try {
+      const [rows] = await db.query('SELECT * FROM students');
+      return rows;
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      throw new Error('Internal Server Error');
+    }
   };
-
 
 
 
