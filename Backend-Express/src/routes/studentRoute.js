16dbf8086a862b893fde0966339cb0dfef89a2b4
Githmaz <Githmaz1@gmail.com>
2023-12-30 20:41:38 +0530

    Complete the patch api for update and added student response util for more meaningful response

--- Backend-Express/src/routes/studentRoute.js
+++ Backend-Express/src/routes/studentRoute.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 router.get('/list',studnetController.getAllStudents); // Get All Students
 
-router.post('/add',studnetController.addStudnet); // add new Student
+router.post('/add',studnetController.addStudnet); // Add new Student
 
-router.delete('/delete/:id',studnetController.deleteStudent); // soft delete Studnet
+router.delete('/delete/:id',studnetController.deleteStudent); // Soft delete Studnet
 router.delete('/delete/perma/:id',studnetController.deleteStudentPermanently); // Permanent delete Studnet
 
 router.put('/restore/:id',studnetController.restoreStudent); // Restore Student that soft Deleted
 
+router.patch('/update/:id',studnetController.updateStudent); // Update Student
 
 module.exports = router;
