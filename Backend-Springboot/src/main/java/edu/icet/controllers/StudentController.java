4164f94fa4baa7b981270da91c88e8aef1469f0f
Githmaz <Githmaz1@gmail.com>
2024-01-03 15:51:22 +0530

    Swagger documentation completed

--- Backend-Springboot/src/main/java/edu/icet/controllers/StudentController.java
+++ Backend-Springboot/src/main/java/edu/icet/controllers/StudentController.java
@@ -4,6 +4,8 @@ import edu.icet.dto.Student;
 import edu.icet.dto.response.StudentResponse;
 import edu.icet.service.StudentService;
 import edu.icet.utility.StudentUtility;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,6 +28,7 @@ public class StudentController {
 
     //------------------ Add a new student ------------------//
     @PostMapping("/SignUp")
+    @Operation(summary = "Add a new student", description = "Endpoint to add a new student.")
     public ResponseEntity<Map<String, Object>> addUser(@RequestBody Student tempStudent) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
                 "Message", "Student added successfully",
@@ -36,7 +39,8 @@ public class StudentController {
 
     //---------------- Set profile picture -----------------//
     @PostMapping("/SetProfilePic")
-    public ResponseEntity<Object> setProfilePic(@RequestParam("image") MultipartFile file, @RequestParam long id) {
+    @Operation(summary = "Set profile picture", description = "Endpoint to set the profile picture of a student.")
+    public ResponseEntity<Object> setProfilePic(@Parameter(description = "Image file", required = true)@RequestParam("image") MultipartFile file, @RequestParam long id) {
         try {
             service.uploadImage(id, file);
             return ResponseEntity.ok().build();
@@ -46,30 +50,35 @@ public class StudentController {
     }
     //---------------- Check phone number -------------------//
     @GetMapping("/CheckPhoneNumber")
+    @Operation(summary = "Check phone number", description = "Endpoint to check the validity of a phone number.")
     public boolean checkPhoneNumber(@RequestParam String phoneNumber) {return StudentUtility.checkPhoneNumber(phoneNumber);
     }
 
     //---------------- Check duplicate username -------------//
     @GetMapping("/CheckDuplicateUsername")
+    @Operation(summary = "Check duplicate username", description = "Endpoint to check if a username already exists.")
     public boolean checkDuplicateUsername(@RequestParam String username) {
         return service.duplicateUsername(username);
     }
 
     //----------------- Check birthday ----------------------//
     @GetMapping("/CheckBirthdayValidation")
+    @Operation(summary = "Check birthday validation", description = "Endpoint to check the validity of a birthday.")
     public boolean checkBirthday(@RequestParam String birthday) {
         return service.checkBirthday(birthday);
     }
 
     //------------------- Student login ----------------------//
     @GetMapping("/Login")
+    @Operation(summary = "Student login", description = "Endpoint for student login.")
     public Student login(@RequestParam String username, @RequestParam String password) {
         return service.studentLoginCheck(username, password);
     }
 
     //------------------- List all students -------------------//
     @GetMapping("/List")
-    public StudentResponse table1() {
+    @Operation(summary = "List all students", description = "Endpoint to retrieve a list of all students.")
+    public StudentResponse table() {
         return  StudentResponse.builder()
                 .studentList(service.getAllUsers())
                 .build();
@@ -77,6 +86,7 @@ public class StudentController {
 
     //---------------- Get profile picture ---------------------//
     @GetMapping("/Image")
+    @Operation(summary = "Get profile picture", description = "Endpoint to retrieve the profile picture of a student.")
     public ResponseEntity<byte[]> getProfilePic(@RequestParam(name = "id", required = true) long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.valueOf("image/png"))
@@ -85,6 +95,7 @@ public class StudentController {
 
     //---------------- Get student by ID -----------------------//
     @GetMapping("/getStudent")
+    @Operation(summary = "Get student by ID", description = "Endpoint to retrieve a student by ID.")
     public Student getStudent(@RequestParam long id) {
         return service.getStudentById(id);
     }
