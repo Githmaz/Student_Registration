c28d09bb07eb201fe5b2ebbaa90d183941b27e43
Githmaz <Githmaz1@gmail.com>
2024-01-14 22:57:47 +0530

    - Nav bar Component completed

--- Frontend-Angular/src/app/app.module.ts
+++ Frontend-Angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegistrationFormComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
