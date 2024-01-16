a62aa557764e78c3b0be9d76c7bdaad21182461e
Githmaz <Githmaz1@gmail.com>
2024-01-17 02:14:07 +0530

    Completed the Table component

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 import { LogInFromComponent } from './components/log-in-from/log-in-from.component';
+import { TableComponent } from './components/table/table.component';
 const routes: Routes = [
   { path: 'registration', component:RegistrationFormComponent },
   { path: 'login', component:LogInFromComponent },
+  { path: 'table', component:TableComponent},
 ];
 
 
